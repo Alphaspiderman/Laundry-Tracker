b3d427a236fe3560dc9e37e19130758569be0f07
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2023-08-27 22:07:38 +0530

    Work on Database

---
+++ lib/models/dbEntry.dart
@@ -0,0 +1,29 @@
+class ItemCard {
+  final int id;
+  final String name;
+  final int state;
+  final String imagePath;
+
+  ItemCard({
+    required this.id,
+    required this.name,
+    required this.state,
+    required this.imagePath,
+  });
+
+  // Make a factory constructor to create a Card from a map
+  factory ItemCard.fromMap(Map<String, dynamic> json) => ItemCard(
+        id: 0,
+        name: json["name"],
+        state: json["status"],
+        imagePath: json["image"],
+      );
+
+  // Make a method to convert a Card to a map
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "status": state,
+        "imagePath": imagePath,
+      };
+}

956d77989858c744126727bd6c003969248fca38
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2023-09-04 21:24:56 +0530

    DB Purge option on Home Screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,23 @@
 import 'package:clothes_tracker/utils/base.dart';
+import 'package:clothes_tracker/utils/db.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final DatabaseHelper dbHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = Get.find();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +30,15 @@ class HomePage extends StatelessWidget {
               Get.snackbar("Test", "Hello!");
             },
             child: const Text('Trigger Snack'),
-          )
+          ),
+          // Add a button to purge the DB
+          OutlinedButton(
+            onPressed: () async {
+              await dbHelper.purgeData();
+              Get.snackbar("Purge", "DB Purged!");
+            },
+            child: const Text('Purge DB'),
+          ),
         ],
       ),
     );

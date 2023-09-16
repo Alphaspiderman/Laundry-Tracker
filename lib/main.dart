2ce92cbdf57c1383e9112857b24e7caa6669fac6
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2023-09-16 22:26:49 +0530

    Splash Screen on Launch

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,17 @@ import 'package:clothes_tracker/utils/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:clothes_tracker/themes/dark.dart';
 import 'package:clothes_tracker/themes/light.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   DBController db = DBController();
   db.initClass();
   Get.put(db.db);
 
-  // await Get.putAsync(() async => DBController().initClass());
   runApp(const MyApp());
 }
 

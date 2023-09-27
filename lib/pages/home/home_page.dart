8264dfd8584f540abd6cfc958c4d25f6b4ded566
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2023-09-27 22:52:21 +0530

    Import/Export data

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:clothes_tracker/navigation/navgation_bar.dart';
 import 'package:clothes_tracker/ui/app_bar.dart';
 import 'package:clothes_tracker/utils/db.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
@@ -9,6 +12,7 @@ class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _HomePageState createState() => _HomePageState();
 }
 
@@ -20,6 +24,101 @@ class _HomePageState extends State<HomePage> {
     FlutterNativeSplash.remove();
     super.initState();
     dbHelper = Get.find();
+  }
+
+  void importData() async {
+    // Pick a file from the device
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result == null) {
+      // User canceled the picker
+      Get.snackbar("Import Failed", "No file selected!");
+      return;
+    }
+    // Get the file path
+    String path = result.files.single.path!;
+    // Confirm its a ZIP file
+    if (!path.endsWith(".zip")) {
+      Get.snackbar("Import Failed", "Invalid file type!");
+      return;
+    } else {
+      // Create a popup with a message and yes-no buttons
+      Get.dialog(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Material(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "Warning",
+                            style: TextStyle(fontSize: 26),
+                          ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            "Local data will be removed before import is attempted!! \nIncase of failed import, exisiting data will not be restored",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 20),
+                          //Buttons
+                          Row(
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Get.back();
+                                  },
+                                  child: const Text(
+                                    'NO',
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () async {
+                                    await dbHelper.importData(File(path));
+                                    Get.back();
+                                    Get.snackbar("Import", "Data Imported!");
+                                  },
+                                  child: const Text(
+                                    'YES',
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  // Function to export all data of the app as ZIP
+  void exportData() async {
+    await dbHelper.exportData();
+
+    Get.snackbar("Export", "Data Exported!");
   }
 
   @override
@@ -52,6 +151,20 @@ class _HomePageState extends State<HomePage> {
                 Get.toNamed("/debug");
               },
               child: const Text('Debug Page'),
+            ),
+            // Add a button to import data
+            OutlinedButton(
+              onPressed: () {
+                importData();
+              },
+              child: const Text('Import Data'),
+            ),
+            // Add a button to export data
+            OutlinedButton(
+              onPressed: () {
+                exportData();
+              },
+              child: const Text('Export Data'),
             ),
           ],
         ),
--- lib/utils/db.dart
+++ lib/utils/db.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'dart:io';
-
 import 'package:clothes_tracker/models/state.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter_archive/flutter_archive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -21,6 +23,11 @@ class DatabaseHelper {
   factory DatabaseHelper() => _instance;
 
   DatabaseHelper._internal();
+
+  Future<void> initClass() async {
+    appDir = await getApplicationDocumentsDirectory();
+    await initDatabase();
+  }
 
   Future<void> initDatabase() async {
     _database = await openDatabase(
@@ -94,7 +101,7 @@ class DatabaseHelper {
   }
 
   // Delete a card
-  Future<void> deleteData(String id) async {
+  Future<void> deleteData(int id) async {
     Database db = await database;
     // Get the data to delete
     final data = await db.query(
@@ -102,18 +109,138 @@ class DatabaseHelper {
       where: 'id = ?',
       whereArgs: [id],
     );
+    //
+    var map = Map<String, dynamic>.from(data[0]);
+    map.addEntries([MapEntry('prepend', appDir!.path)]);
     // Convert to DbEntry
-    final dataEntry = DbEntry.fromMap(data[0]);
+    final dataEntry = DbEntry.fromMap(map);
     // Delete the data
     await db.delete(
       'clothes',
       where: 'id = ?',
       whereArgs: [id],
     );
+    await File(dataEntry.imagePath).delete();
+  }
 
-    final appDir = await getApplicationDocumentsDirectory();
-    final imagePath = join(appDir.path, '${dataEntry.imagePath}.png');
-    await File(imagePath).delete();
+  // Import data
+  Future<void> importData(File file) async {
+    // Purge the database
+    await purgeData();
+
+    // Extract the ZIP file
+    await ZipFile.extractToDirectory(
+      zipFile: file,
+      destinationDir: Directory(join(appDir!.path, "import")),
+    );
+
+    // Declare the directory for the imported data
+    final importDir = Directory(join(appDir!.path, "import"));
+    final importFile = File(join(importDir.path, "data.json"));
+
+    // Read JSON file and get a list
+    final data = await importFile.readAsString();
+    List jsondata = json.decode(data);
+
+    // Convert to List
+    List<DbEntry> dataList = jsondata.map((e) => DbEntry.fromJson(e)).toList();
+
+    // Declare the directory for images
+    final imagesDir = Directory(join(appDir!.path, "images"));
+    final importImagesDir = Directory(importDir.path);
+
+    // Create directory if it doesn't exist
+    if (!imagesDir.existsSync()) {
+      await imagesDir.create(recursive: true);
+    }
+
+    Database db = await database;
+    // Insert data into the database and copy image
+    for (DbEntry data in dataList) {
+      await db.insert('clothes', {
+        'name': data.name,
+        'state': data.state.index,
+        'image_path': data.imagePath,
+      });
+
+      final importImagePath = importImagesDir.path + data.imagePath;
+      final saveImagePath = appDir!.path + data.imagePath;
+      await File(importImagePath).copy(saveImagePath);
+    }
+  }
+
+  // Export data as a ZIP
+  Future<void> exportData() async {
+    // Declare folder for export
+    final exportDir = Directory(join(appDir!.path, "export"));
+    final exportImagesDir = Directory(join(exportDir.path, "images"));
+
+    // Delete directory if it exists
+    if (exportDir.existsSync()) {
+      await exportDir.delete(recursive: true);
+    }
+    // Create directory
+    await exportImagesDir.create(recursive: true);
+
+    // Get database
+    Database db = await database;
+
+    // Query for all entries
+    final List<Map<String, dynamic>> maps = await db.query('clothes');
+    List<DbEntry> dataList = generateList(maps, prepend: false);
+
+    // Convert to JSON
+    String jsonData = jsonEncode(dataList.map((e) => e.toJson()).toList());
+
+    // Write JSON to file
+    final exportFile = File(join(exportDir.path, "data.json"));
+    await exportFile.writeAsString(jsonData);
+
+    // Declare the directory for images
+    final imagesDir = Directory(join(appDir!.path));
+
+    // Copy all images to export directory
+    for (var data in dataList) {
+      // Declare the path for the image
+      String exportImagePath = exportDir.path + data.imagePath;
+
+      // Access file to copy
+      File f = File(imagesDir.path + data.imagePath);
+
+      print(f.path);
+      print(exportImagePath);
+
+      // Copy the file
+      if (await f.exists()) {
+        await f.copy(exportImagePath);
+      } else {
+        print(f.path);
+      }
+    }
+
+    // Create a ZIP file
+    final zipFile = File(join(appDir!.path, "export.zip"));
+    await ZipFile.createFromDirectory(
+      sourceDir: exportDir,
+      zipFile: zipFile,
+    );
+
+    // Delete the export directory
+    await exportDir.delete(recursive: true);
+
+    // Ask where to save the file
+    final saveFilePath = await FilePicker.platform.getDirectoryPath();
+    if (saveFilePath == null) {
+      // User canceled the picker
+      return;
+    }
+    // Declare file to save
+    String now = DateTime.now().toString().split(".")[0].replaceAll(":", "-");
+    File saveFile = File(join(saveFilePath, "export-$now.zip"));
+    // Save File
+    await zipFile.copy(saveFile.path);
+    // Delete the temp file
+    await zipFile.delete();
   }
 
   // Purge the database
@@ -127,11 +254,12 @@ class DatabaseHelper {
     await Directory(imagePath).create();
   }
 
-  List<DbEntry> generateList(List<Map<String, dynamic>> maps) {
+  List<DbEntry> generateList(List<Map<String, dynamic>> maps,
+      {bool prepend = true}) {
     return List.generate(maps.length, (index) {
       // Create a editable copy of the map
       var map = Map<String, dynamic>.from(maps[index]);
-      map.addEntries([MapEntry('prepend', appDir!.path)]);
+      map.addEntries([MapEntry('prepend', prepend ? appDir!.path : '')]);
       return DbEntry.fromMap(map);
     });
   }

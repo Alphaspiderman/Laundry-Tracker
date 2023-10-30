918db19825fd3b9d72fedd05b8937786f0b9e4bf
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2023-10-30 21:19:12 +0530

    Allow option to click an image

--- lib/views/imgpicker.dart
+++ lib/views/imgpicker.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
@@ -20,52 +19,68 @@ class ImagePickerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () async {
-        XFile? imageFile = await pickImage(context);
-        if (imageFile != null) {
-          // Get the application directory
-          final appDir = await getApplicationDocumentsDirectory();
-
-          // Create a unique image name
-          String safeString = getSafe();
-          String imageName = '${imageFile.path.hashCode}-$safeString.png';
-
-          // Create folders if do not exist
-          await Directory(join(appDir.path, 'temp')).create(recursive: true);
-          await Directory(join(appDir.path, 'images')).create(recursive: true);
-
-          // Create the path for checking
-          String finalImagePath = join(appDir.path, 'images', imageName);
-
-          // Make sure the file does not exist in final location
-          var file = File(finalImagePath);
-          // If the file exists, create a new name
-          if (await file.exists()) {
-            String safeString = DateTime.now().microsecond.toString() +
-                DateTime.now().second.toString() +
-                DateTime.now().minute.toString();
-            imageName = '${imageFile.path.hashCode}-$safeString.png';
-          }
-
-          // Create the final paths for the temp and final locations
-          String tempImagePath = join(appDir.path, 'temp', imageName);
-
-          // Move file to temp location
-          await imageFile.saveTo(tempImagePath);
-          onDataReceived(appDir.path, imageName);
-        }
-      },
-      child: const Text('Pick Image'),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () async {
+            XFile? imageFile = await pickImage(context, ImageSource.gallery);
+            await processFile(imageFile);
+          },
+          child: const Text('Pick Image'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            XFile? imageFile = await pickImage(context, ImageSource.camera);
+            await processFile(imageFile);
+          },
+          child: const Text('Take an Image'),
+        ),
+      ],
     );
   }
 
-  Future<XFile?> pickImage(BuildContext context) async {
+  Future<void> processFile(XFile? imageFile) async {
+    if (imageFile != null) {
+      // Get the application directory
+      final appDir = await getApplicationDocumentsDirectory();
+
+      // Create a unique image name
+      String safeString = getSafe();
+      String imageName = '${imageFile.path.hashCode}-$safeString.png';
+
+      // Create folders if do not exist
+      await Directory(join(appDir.path, 'temp')).create(recursive: true);
+      await Directory(join(appDir.path, 'images')).create(recursive: true);
+
+      // Create the path for checking
+      String finalImagePath = join(appDir.path, 'images', imageName);
+
+      // Make sure the file does not exist in final location
+      var file = File(finalImagePath);
+      // If the file exists, create a new name
+      if (await file.exists()) {
+        String safeString = DateTime.now().microsecond.toString() +
+            DateTime.now().second.toString() +
+            DateTime.now().minute.toString();
+        imageName = '${imageFile.path.hashCode}-$safeString.png';
+      }
+
+      // Create the final paths for the temp and final locations
+      String tempImagePath = join(appDir.path, 'temp', imageName);
+
+      // Move file to temp location
+      await imageFile.saveTo(tempImagePath);
+      onDataReceived(appDir.path, imageName);
+    }
+  }
+
+  Future<XFile?> pickImage(BuildContext context, ImageSource source) async {
     final picker = ImagePicker();
 
     try {
       final XFile? pickedFile =
-          await picker.pickImage(source: ImageSource.gallery);
+          await picker.pickImage(source: source, imageQuality: 100);
 
       if (pickedFile != null) {
         return pickedFile;

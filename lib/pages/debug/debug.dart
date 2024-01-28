2e4b5a7b0e47e4c17e3c02b4f020c091b4919f4f
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-01-28 21:19:25 +0530

    Cleanup Debug View

--- lib/pages/debug/debug.dart
+++ lib/pages/debug/debug.dart
@@ -2,7 +2,6 @@ import 'package:clothes_tracker/navigation/navgation_bar.dart';
 import 'package:clothes_tracker/pages/debug/debug_controller.dart';
 import 'package:clothes_tracker/ui/app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class DebugPage extends StatefulWidget {
   const DebugPage({super.key});
@@ -21,15 +20,6 @@ class _DebugPageState extends State<DebugPage> {
     debugController.addListener(() {
       setState(() {});
     });
-  }
-
-  void _hasData() {
-    Get.snackbar(
-      'Success',
-      'Data saved successfully',
-      duration: const Duration(seconds: 1),
-    );
-    setState(() {});
   }
 
   @override
